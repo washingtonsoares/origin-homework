9313c2e9808ec209a2adcb0e735cae537a23bb3d
Washington Soares <washingtonudi@gmail.com>
2021-02-27 23:11:08 -0300

    Fix minimum fraction digits

--- src/pages/SavingGoal/helpers.ts
+++ src/pages/SavingGoal/helpers.ts
@@ -1,4 +1,8 @@
 export function getMonthlyDeposits(totalAmount = 0, monthsAmount: number) {
+  if (monthsAmount <= 0) {
+    return 0;
+  }
+
   const monthlyDeposits = totalAmount / monthsAmount;
 
   return Math.ceil(monthlyDeposits);
--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -2,6 +2,6 @@ export function formatCurrency(value: number) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-    maximumFractionDigits: 0
+    minimumFractionDigits: 0
   }).format(value);
 }
